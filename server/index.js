3e6ee17f9f72587f6e7b8d856a7b79a3fba7609c
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-05-08 05:17:19 -0300

    update: server config

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const PORT = process.env.PORT || 6000
+const {connectDB} = require("./db/db")
+const referrerRouter = require("./routes/referrerRouter")
+const userRouter =require("./routes/userRouter")
+connectDB()
+app.use(cors())
+app.use(express.json())
+
+app.use("/user",userRouter)
+app.use("/referrer",referrerRouter)
+
+app.listen(PORT,(req,res)=>{
+    res.send(`Listening PORT ${PORT}`)
+})
+
+process.on("unhandledRejection",(err,promise)=>{
+    console.log(`Connection error ${err}`)
+    server.close(()=> process.exit(1))
+})
