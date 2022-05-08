ac1c287e3020ac7eb8b451ff49a9a0fb6919c699
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-05-08 05:17:30 -0300

    update: midd auth

---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+const {User} = require("../db/models/User")
+
+const authUser = async(req,res,next)=>{
+    let token;
+    let auth = req.get("Authorization")
+    if(auth && auth.startsWith("Bearer")) token= auth.split(" ")[1]
+    if(!token || token=="") return res.status(404).json({error:true,message:"Not authorized to access"})
+    try{
+        let decodifed = jwt.verify(token,process.env.JWT_SECRET)
+        const user = await User.findById(decodifed.id)
+        if(!user) return errorResponse("Not authorizated to access",404,res)
+        req.user = user
+        next()
+    }catch(err){
+        return res.status(404).json({error:true,message:err.message})
+    }
+}
+
+
+module.exports = {authUser}
