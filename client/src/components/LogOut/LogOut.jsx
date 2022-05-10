37fc50ef81b16e7759fe36e5ad368c93df71cfad
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-05-10 12:40:51 -0300

    fix: logout go to home

--- client/src/components/LogOut/LogOut.jsx
+++ client/src/components/LogOut/LogOut.jsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react'
 import AuthContext from '../../context/authContext'
+import {useNavigate} from "react-router-dom"
 import "./LogOut.scss"
 const LogOut = () => {
  const {setAuth} = useContext(AuthContext)
-
+ const navigate = useNavigate()
   const handleLogOut = ()=>{
       localStorage.removeItem("user-currency")
       setAuth(null)
+      navigate("/")
   }
   return (
    <button className='logout' onClick={()=>handleLogOut()}>Log Out</button>
